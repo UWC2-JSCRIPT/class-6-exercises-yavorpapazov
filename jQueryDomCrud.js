c9c728a07eeb6d286699dbaa663faf51cf8f15fe
Yavor Papazov <y_papaz@yahoo.com>
2022-11-23 12:24:32 -0500

    complete in-class exercises

--- jQueryDomCrud.js
+++ jQueryDomCrud.js
@@ -1,15 +1,29 @@
-// Create a new <a> element containing the text "Buy Now!"
-// with an id of "cta" after the last <p>
-
-// Access (read) the data-color attribute of the <img>,
-// log to the console
-
-// Update the third <li> item ("Turbocharged"),
-// set the class name to "highlight"
-
-// Remove (delete) the last paragraph
-// (starts with "Available for purchase now…")
-
-// Create a listener on the "Buy Now!" link that responds to a click event.
-// When clicked, the the "Buy Now!" link should be removed
-// and replaced with text that says "Added to cart"
+$(document).ready(function() {
+    // Create a new <a> element containing the text "Buy Now!"
+    // with an id of "cta" after the last <p>
+    let $a = $('<a>')
+    $a.text('Buy Now!')
+    $a.attr('id', 'cta')
+    $('main').append($a)
+    // Access (read) the data-color attribute of the <img>,
+    // log to the console
+    let $img = $('img')
+    console.log($img.data('color'))
+    // Update the third <li> item ("Turbocharged"),
+    // set the class name to "highlight"
+    let $li = $('li:nth-child(3)')
+    $li.addClass('highlight')
+    // Remove (delete) the last paragraph
+    // (starts with "Available for purchase now…")
+    $('p').remove()
+    // Create a listener on the "Buy Now!" link that responds to a click event.
+    // When clicked, the the "Buy Now!" link should be removed
+    // and replaced with text that says "Added to cart"
+    $('#cta').on('click', function() {
+        let $this = $(this)
+        $this.remove()
+        let $h3= $('<h3>')
+        $h3.text('Added to cart')
+        $('main').append($h3)
+    })
+})
--- stoppingBehavior.js
+++ stoppingBehavior.js
@@ -5,6 +5,15 @@ document.getElementById('cat').addEventListener('click', () => {
 
 // When clicked, "More info" link should alert "Here's some info"
 // instead of going to a new webpage
-
+let aElement = document.querySelector('#more-info')
+aElement.addEventListener('click', e => {
+  e.preventDefault()
+  alert(`Here's some info`)
+})
 // When the bark button is clicked, should alert "Bow wow!"
 // Should *not* alert "meow"
+let btnElement = document.querySelector('#dog')
+btnElement.addEventListener('click', e => {
+  e.stopPropagation()
+  alert('Bow wow!')
+})
